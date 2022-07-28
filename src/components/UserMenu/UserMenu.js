19d4cbc217324259390079c3aeee7097355f612d
Slendermanyshka <slendermanyshka@gmail.com>
2022-07-28 22:36:40 +0300

    initial commit

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authOperations } from '../../redux/auth';
+
+import s from './UserMenu.module.css';
+
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUsername);
+
+  return (
+    <div className={s.container}>
+      <span className={s.name}>{name}</span>
+      <button
+        className={s.button}
+        onClick={() => dispatch(authOperations.logOut())}
+        type="button"
+        title="Log out"
+        aria-label="Log out"
+      >
+        Log out
+      </button>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import s from './HomePage.module.css';
+
+export default function HomePage() {
+  return (
+    <main>
+      <div className={s.container}>
+        <h2 className={s.title}>Phonebook App</h2>
+      </div>
+    </main>
+  );
+}
